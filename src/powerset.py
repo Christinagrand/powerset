75517c498090b9c03bd46f9d370b164b212c89fb
Christinagrand <christinagrand@hotmail.com>
2023-09-13 14:33:23 +0200

    prøve

--- src/powerset.py
+++ src/powerset.py
@@ -9,4 +9,14 @@ def power(x: list[Any]) -> list[list[Any]]:
 
     Returns the power-set of x as a list of lists.
     """
-    return []  # FIXME: you need to add some code here.
+    subset = [[]]
+    for i in range(len(x)+1):
+        for j in range(i+1,len(x)+1):
+            ny = x[i:j]
+            subset.append(ny)
+    return subset  # FIXME: you need to add some code here.
+
+# print(power([1,2,3]))
+
+# y = [1,2]
+# print(y[0:1])
